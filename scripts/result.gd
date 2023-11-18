52d1216e1febbac3af2fc5c4299a5b564d95d86f
Ynda Jas <yndajas@gmail.com>
2023-11-18 11:54:53 +0000

    Add continue prompt to result screen

--- scripts/result.gd
+++ scripts/result.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 var stars_filled: int = 0
+@onready var continue_prompt: RichTextLabel = $ContinuePrompt
+@onready var continue_prompt_timer: Timer = $ContinuePrompt/Timer
 @onready var filled_stars: Array[Sprite2D] = [$FilledStar0, $FilledStar1, $FilledStar2]
 @onready var sfx_player: AudioStreamPlayer = $StarFillTimer/SfxPlayer
 @onready var star_fill_timer: Timer = $StarFillTimer
@@ -8,17 +10,27 @@ var stars_filled: int = 0
 
 func _ready() -> void:
 	print_debug(Global.level_scores[Global.current_level])
-	start_timer_if_fill_needed()
+	start_timer()
 
 
-func start_timer_if_fill_needed() -> void:
-	if stars_filled < Global.level_scores[Global.current_level]:
-		var wait_time: float = 0.75 if not stars_filled else 0.5
-		star_fill_timer.start(wait_time)
+func fill_needed() -> bool:
+	return stars_filled < Global.level_scores[Global.current_level]
+
+
+func start_timer() -> void:
+	var wait_time: float = 0.75 if not stars_filled else 0.5
+	star_fill_timer.start(wait_time)
+
+
+func _on_continue_prompt_timer_timeout() -> void:
+	continue_prompt.visible = true
 
 
 func _on_star_fill_timer_timeout() -> void:
 	filled_stars[stars_filled].visible = true
 	sfx_player.play()
 	stars_filled += 1
-	start_timer_if_fill_needed()
+	if fill_needed():
+		start_timer()
+	else:
+		continue_prompt_timer.start()
